60365d9d3495ca95397cc49bc172924ca6f9332d
Liam Morgan <Liamsipad@outlook.com>
2017-01-23 13:40:52 +0100

    Uppgift 3

--- 23 01 17/Assets/Uppgift3.cs
+++ 23 01 17/Assets/Uppgift3.cs
@@ -4,9 +4,16 @@ using System.Collections;
 public class Uppgift3 : MonoBehaviour {
     public int valA = 5;
     public int valB = 10;
+
+    int Metod (int a, int b)
+    {
+        //Debug.Log(valA + valB);
+        return a + b;
+
+    }
 	// Use this for initialization
 	void Start () {
-        Debug.Log(valA + valB);
+        Debug.Log(Metod(valA, valB));
        
 	}
 	
