71228a96e414a713f8abadb0678b37328f0110e9
Liam Morgan <Liamsipad@outlook.com>
2017-01-23 12:57:09 +0100

    Uppgift 2

---
+++ 23 01 17/Assets/Uppgift2.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Uppgift2 : MonoBehaviour {
+    public int valA = 5;
+    public int valB = 10;
+
+	// Use this for initialization
+	void Start () {
+	if (valA > valB)
+        {
+            Debug.Log(valA);
+        }
+    if (valB > valA)
+        {
+            Debug.Log(valB);
+        }
+    if (valA == valB)
+        {
+            Debug.Log("The Same Value");
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ 23 01 17/Assets/Uppgift3.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Uppgift3 : MonoBehaviour {
+    public int valA = 5;
+    public int valB = 10;
+	// Use this for initialization
+	void Start () {
+        Debug.Log(valA + valB);
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
