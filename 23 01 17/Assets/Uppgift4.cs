d94e353588b513ffab8696fe24f03f036c10f6ce
Liam Morgan <Liamsipad@outlook.com>
2017-01-30 11:28:06 +0100

    Uppgift 4

---
+++ 23 01 17/Assets/Uppgift4.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Uppgift4 : MonoBehaviour {
+    public float valA;
+    public float valB;
+    public float valC;
+
+	// Use this for initialization
+	void Start () {
+        //valA = valB - 5;
+	if(valC < valB || valC < valA)
+        {
+            Debug.Log("In The Middle");
+        }
+    else
+        {
+            Debug.Log("Not In the Middle");
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
